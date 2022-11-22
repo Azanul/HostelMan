7fd3a898a2a324f5f4ffe99b28a7d07905bb0246
Azanul <azanulhaque@gmail.com>
2022-11-23 02:02:10 +0530

    Upload files and array - multipart/form API

--- application/main.go
+++ application/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,12 +18,31 @@ func main() {
 		"manu":   "4321",
 	}))
 
-	// /user/user endpoint
+	// /user/apply endpoint
 	// hit "localhost:8080/user/user
-	authorized.GET("/user", func(c *gin.Context) {
+	authorized.POST("/apply", func(c *gin.Context) {
 		// get user, it was set by the BasicAuth middleware
-		user := c.MustGet(gin.AuthUserKey).(string)
-		c.JSON(http.StatusOK, gin.H{"user": user})
+		// user := c.MustGet(gin.AuthUserKey).(string)
+
+		name := c.PostForm("name")
+
+		// Multipart form
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			return
+		}
+		files := form.File["upload[]"]
+
+		for _, file := range files {
+			filename := filepath.Base(file.Filename)
+			if err := c.SaveUploadedFile(file, filename); err != nil {
+				c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+				return
+			}
+		}
+
+		c.String(http.StatusOK, "Uploaded successfully %d files with fields name=%s and email=%s.", len(files), name, form.Value["quotas[]"])
 	})
 
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
