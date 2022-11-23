d87e8dd838b5744bb179d7670e3c2ba035112a75
Azanul <azanulhaque@gmail.com>
2022-11-23 10:12:49 +0530

    Variable & File naming update

--- application/main.go
+++ application/main.go
@@ -19,30 +19,27 @@ func main() {
 	}))
 
 	// /user/apply endpoint
-	// hit "localhost:8080/user/user
-	authorized.POST("/apply", func(c *gin.Context) {
+	authorized.POST("/apply", func(ctx *gin.Context) {
 		// get user, it was set by the BasicAuth middleware
-		// user := c.MustGet(gin.AuthUserKey).(string)
-
-		name := c.PostForm("name")
+		user := ctx.MustGet(gin.AuthUserKey).(string)
 
 		// Multipart form
-		form, err := c.MultipartForm()
+		form, err := ctx.MultipartForm()
 		if err != nil {
-			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			ctx.String(http.StatusBadRequest, "get form err: %s", err.Error())
 			return
 		}
 		files := form.File["upload[]"]
 
 		for _, file := range files {
 			filename := filepath.Base(file.Filename)
-			if err := c.SaveUploadedFile(file, filename); err != nil {
-				c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+			if err := ctx.SaveUploadedFile(file, "recieved_"+filename); err != nil {
+				ctx.String(http.StatusBadRequest, "upload file err: %s", err.Error())
 				return
 			}
 		}
 
-		c.String(http.StatusOK, "Uploaded successfully %d files with fields name=%s and email=%s.", len(files), name, form.Value["quotas[]"])
+		ctx.String(http.StatusOK, "Uploaded successfully %d files with fields name=%s and email=%s.", len(files), user, form.Value["quotas[]"])
 	})
 
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
